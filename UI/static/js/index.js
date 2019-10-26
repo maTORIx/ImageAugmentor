a85a0db90ec764140311e6f1e0edf86d858a24be
maTORIx <matorixmatrix@gmail.com>
2019-10-26 19:01:01 +0900

    tmp

--- UI/static/js/index.js
+++ UI/static/js/index.js
@@ -1,6 +1,7 @@
 
 const API_ROOT = "/api"
-const DEFAULT_AUGMENT_OPTIONS = ["crop", "crop", "flip-horizontal", "flip-vertical"]
+const DEFAULT_AUGMENT_OPTIONS = ["crop"]
+const PARAM_TYPES = ["range", "select"]
 
 document.addEventListener("DOMContentLoaded", function() {
     let app = new Vue({
@@ -11,6 +12,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 results: [],
                 selected_dataset: null,
                 selected_augment_options: [],
+                augment_value: {},
                 augment_option_choises: {},
                 augment_option_dialog: false,
                 augment_sample_data: [],
@@ -49,11 +51,37 @@ document.addEventListener("DOMContentLoaded", function() {
             selectDataset(id) {
                 this.selected_dataset = id
             },
+            setAugmentOptionValue(augment_option_choises) {
+                let result = {}
+                for (option_key in augment_option_choises) {
+                    let option = augment_option_choises[option_key]
+                    result[option_key] = {}
+                    result[option_key].enable = false
+                    for (param of option.params) {
+                        if (param.type == "range") {
+                            result[option_key][param.name] = {
+                                min: param.min_default,
+                                max: param.max_default
+                            }
+                        } else if (param.type == "select") {
+                            result[option_key][param.name] = param.default
+                        }
+                    }
+                }
+                this.augment_value = result
+            },
             addAugmentOption(option_name) {
                 if (!Object.keys(this.augment_option_choises).includes(option_name)) {
                     this.createError(new Error("Invalid option. Augment option doesn't include this option."))
                 }
                 this.selected_augment_options.push(option_name)
+                this.augment_value[option_name].enable = true
+            },
+            deleteAugmentOption(option_name) {
+                const target_idx = this.selected_augment_options.indexOf(option_name)
+                if (target_idx < 0) this.createError(new Error("Invalid option. selected option doesn't found."))
+                this.selected_augment_options.splice(target_idx, 1)
+                this.augment_value[option_name].enable = false
             },
             toggleAugmentOptionDialog() {
                 const augment_option_dialog = document.querySelector("#augment-choises-dialog")
@@ -72,6 +100,16 @@ document.addEventListener("DOMContentLoaded", function() {
                 } else {
                     augment_sample_dialog.close()
                 }
+            },
+            deleteDataset(id) {
+                const target_idx = this.datasets.findIndex((val) => val.id === id)
+                if (target_idx < 0) this.createError(new Error("Dataset not found. Please reload webpage and retry."))
+                this.datasets.splice(target_idx, 1)
+            }
+        },
+        computed: {
+            not_selected_augment_options() {
+                return Object.keys(this.augment_option_choises).filter(val => !this.selected_augment_options.includes(val))
             }
         },
         async mounted() {
@@ -82,10 +120,11 @@ document.addEventListener("DOMContentLoaded", function() {
                     this.loadAugmentOptionChoises()
                 ])
                 this.selected_augment_options = DEFAULT_AUGMENT_OPTIONS
+                this.setAugmentOptionValue(this.augment_option_choises)
                 console.log(await this.augment_option_choises)
             } catch(e) {
                 this.error.push(e.toString())
             }
-        }
+        },
     })
 })
